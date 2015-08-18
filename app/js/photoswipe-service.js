9c541e7050d19e7bfe44790d700edb74f4648d90
Edward Turner <eddturner@ebi.ac.uk>
2015-08-18 09:55:44 +0100

    try some other pics

--- app/js/photoswipe-service.js
+++ app/js/photoswipe-service.js
@@ -1,28 +1,44 @@
-showGallery = function() {
+showGallery = function () {
     var pswpElement = document.querySelectorAll('.pswp')[0];
 
-            // build items array
-            var items = [
-            {
-                src: 'http://a1.dspncdn.com/media/692x/9b/1d/44/9b1d44b93d6a3c06b5775044cb1af260.jpg',
-                w: 600,
-                h: 400
-            },
-            {
-                src: 'https://placekitten.com/1200/900',
-                w: 1200,
-                h: 900
-            }
-            ];
+    // build items array
+    var items = [
+        {
+            src: 'img/dsc_0277_15160311526_o.jpg',
+            w: 4928,
+            h: 3264
+        },
+        {
+            src: 'img/DSC_0633.JPG',
+            w: 4928,
+            h: 3264
+        },
+        {
+            src: 'http://a1.dspncdn.com/media/692x/9b/1d/44/9b1d44b93d6a3c06b5775044cb1af260.jpg',
+            w: 600,
+            h: 400
+        },
+        {
+            src: 'https://placekitten.com/1200/900',
+            w: 1200,
+            h: 900
+        }
+    ];
 
-            // define options (if needed)
-            var options = {
-                // optionName: 'option value'
-                // for example:
-                index: 0 // start at first slide
-            };
+    // define options (if needed)
+    var options = {
+        // optionName: 'option value'
+        index: 0,       // start at first slide
+        shareEl: false,  // don't want it to be shareable
+        shareButtons: [
+            //{id:'facebook', label:'Share on Facebook', url:'https://www.facebook.com/sharer/sharer.php?u={{url}}'},
+            //{id:'twitter', label:'Tweet', url:'https://twitter.com/intent/tweet?text={{text}}&url={{url}}'},
+            //{id:'pinterest', label:'Pin it', url:'http://www.pinterest.com/pin/create/button/?url={{url}}&media={{image_url}}&description={{text}}'},
+            {id:'download', label:'Download image', url:'{{raw_image_url}}', download:true}
+        ]
+    };
 
-            // Initializes and opens PhotoSwipe
-            var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-            gallery.init();
-        };
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
