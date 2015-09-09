5e979720630b3f2f8d2c4093888e0981bbc7aaa6
Edward Turner <eddturner@ebi.ac.uk>
2015-09-09 11:28:32 +0100

    adding flickr access scripts to generate javascript function, which returns info for photoswipe item array

--- app/js/photoswipe-service.js
+++ app/js/photoswipe-service.js
@@ -2,15 +2,7 @@ showGallery = function () {
     var pswpElement = document.querySelectorAll('.pswp')[0];
 
     // build items array
-    var items = [
-        {src: 'img/dsc_0277_15160311526_o.jpg', w: 4928, h: 3264}
-        , {src: 'img/DSC_0633.JPG', w: 4928, h: 3264}
-        , {src: 'img/IMG_20150502_113628295.jpg', w: 2592, h: 1456}
-        , {src: 'img/IMG_20150502_113712641.jpg', w: 1456, h: 2592}
-        , {src: 'img/IMG_20150605_231335239.jpg', w: 1456, h: 2592}
-        , {src: 'img/IMG_20150615_185413911.jpg', w: 1456, h: 2592}
-        , {src: 'img/IMG_20150615_185430726.jpg', w: 2592, h: 1456}
-    ];
+    var items = getPsItems();
 
     // define options (if needed)
     var options = {
