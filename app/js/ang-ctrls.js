2d55c798af280626643b09886fe2e5d649532ee3
Edward Turner <eddturner@ebi.ac.uk>
2015-09-11 09:48:26 +0100

    linked to image gallery that is laid out automatically with collagePlus. Need to make this work on $location changes, not full page refresh. Should move dom manipulation to directive.

--- app/js/ang-ctrls.js
+++ app/js/ang-ctrls.js
@@ -3,6 +3,28 @@ var controllers = angular.module('ctrlsModule', []);
 controllers.controller('HomeCtrl', ['$scope', function($scope) {
     $scope.started = true;
 }]);
-controllers.controller('GalleryCtrl', ['$scope', function($scope) {
+controllers.controller('GalleryCtrl', ['$scope', '$document', function($scope, $document) {
     $scope.started = true;
+
+    console.log("gallery controller started");
+    $scope.mediumPictures = getPsMediumItems();
+    console.log("mediumPictures = " + $scope.mediumPictures);
+
+    function collage() {
+        $('.Collage').removeWhitespace().collagePlus(
+            {
+                'fadeSpeed'     : 1500,
+                'targetHeight'  : 300
+            }
+        );
+    };
+
+    angular.element(window).load(function () {
+        $document.ready(function () {
+            console.log("document in gallery is ready.");
+            collage();
+            $('.Collage').collageCaption();
+        });
+    });
+
 }]);
