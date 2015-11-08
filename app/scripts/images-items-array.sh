0717448569a596ad2432315b7068b939ebd3782f
Edward Turner <eddturner@gmail.com>
2015-11-08 20:36:50 +0000

    moving to use gh-pages

--- app/scripts/images-items-array.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-separator="";
-
-for f in ../img/*.[jJ][pP][gG]; do
-#	echo "processing $f";
-	gm identify $f -format "$separator{ src: 'img/%f', w: %w, h: %h }"
-	separator=","
-done
